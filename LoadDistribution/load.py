065039cddf3084125919f13e53f9083a7d08532d
acruis <datsquall@gmail.com>
2017-03-02 15:33:30 +0800

    Load balancing

--- LoadDistribution/load.py
+++ LoadDistribution/load.py
@@ -13,6 +13,13 @@ def make0to1(i):
 	return ceil( make0toinf(i) / (make0toinf(i) + 1) )
 
 def k(i):
-	return ceil( x / p ) - make0to1(i)
+	return ceil( x / p ) - makem0or1(m) * make0to1(i)
 
+def j(i):
+	return ceil( x / p ) * i - makem0or1(m) * (((i - m) + abs(i - m)) / 2)
+
+def makem0or1(i):
+	return ceil( i / (i + 1) )
+
+print [j(i) for i in range(p)]
 print [k(i) for i in range(p)]
