d33aa8730ffdb0a4c122c1ed50bcada5c6cccc6d
acruis <datsquall@gmail.com>
2017-06-22 14:54:19 +0700

    Add results

--- Bitshifts/shift.c
+++ Bitshifts/shift.c
@@ -3,7 +3,7 @@
 int main() {
 	int c = -101;
 	int b = (int)(((unsigned int)c) << 16);
-	printf("%d\n", c);
-	printf("%d\n", b);
+	printf("%d\n", c); // -101
+	printf("%d\n", b); // -6619136; still negative
 	return 0;
 }
